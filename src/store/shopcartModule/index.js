795b4b6ed662209ab1d41f0336b40fccb896e76f
listennn <listennn@Listens-MacBook-Pro.local>
2020-08-28 20:53:22 +0800

    add delete function

--- src/store/shopcartModule/index.js
+++ src/store/shopcartModule/index.js
@@ -12,15 +12,22 @@ const shopcartModule = {
     setShopcartItems({ commit }, datas) {
       commit('SET_SHOPCART_ITEMS', datas);
     },
+    deleteItem({ commit }, id) {
+      commit('DELETE_SHOPCART_ITEM', id);
+    },
     deleteAllItem({ commit }) {
-      commit('DELETE_SHOPCART_ITEM');
+      commit('DELETE_SHOPCART_ITEMS');
     },
   },
   mutations: {
     SET_SHOPCART_ITEMS(state, datas) {
       Vue.set(state, 'shopcartItems', [...datas]);
     },
-    DELETE_SHOPCART_ITEM(state) {
+    DELETE_SHOPCART_ITEM(state, index) {
+      state.shopcartItems.splice(index, 1);
+      Vue.set(state, 'shopcartItems', state.shopcartItems);
+    },
+    DELETE_SHOPCART_ITEMS(state) {
       Vue.set(state, 'shopcartItems', []);
     },
   },
