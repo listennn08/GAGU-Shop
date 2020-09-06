ad66a467d47b14f794cf046227695cbc691ad619
listennn <listennn@Listens-MacBook-Pro.local>
2020-09-06 17:54:11 +0800

    fix rwd and click router

--- src/components/RandomRecommend.vue
+++ src/components/RandomRecommend.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
-  .box.is-shadowless.mt-1
-    .subtitle.is-4.is-clearfix.has-text-weight-bold.has-text-centered {{ title }}
-    .columns.is-multiline
-      .column.is-third-four(
+  .box.is-shadowless.mt-1.cursor
+    .subtitle.is-4.is-clearfix.has-text-weight-bold(:class="titleSide") {{ title }}
+    .columns.is-multiline(:class="center")
+      .column.is-one-fifth-fullhd.is-one-quarter-widescreen.is-one-third-desktop(
+        class="is-two-fifths-tablet"
         v-for="data in recommend"
-        @click="$router.push(`product/${data.id}`)"
+        @click="goTo(data.id)"
       )
-        .card
+        .card.rdn-card
           figure.image.1by1
             img(:src="data.imageUrl[0]")
           .card-info.mx-3
@@ -16,7 +17,7 @@
               del.ml-1: small.has-text-lightgray.is-font-raleway
                 | {{ data.origin_price | cash }}
             .fullheight.has-centered
-              button.button.is-primary(@click.stop="$router.push(`product/${data.id}`)") 查看更多
+              button.button.is-primary(@click.stop="goTo(data.id)") 查看更多
 </template>
 <script>
 import { getAllProducts } from '@/apis/frontend';
@@ -28,31 +29,90 @@ export default {
       type: String,
       required: true,
     },
+    titleSide: {
+      type: String,
+      default: 'has-text-centered',
+    },
+    centered: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+    },
   },
   data() {
     return {
       recommend: [],
     };
   },
+  computed: {
+    center() {
+      return (this.centered ? 'is-centered' : '');
+    },
+  },
   created() {
     this.getRandomProducts();
+  },
+  watch: {
+    '$route.params': {
+      handler() {
+        if (this.$route.params !== this.id) {
+          this.recommend = [];
+          this.getRandomProducts();
+        }
+      },
+    },
   },
   methods: {
     async getRandomProducts() {
       const resp = await getAllProducts();
       const { data } = resp.data;
-      const { total } = resp.data.meta.pagination;
-      const randomArray = Array.from({ length: total })
-        .map((el, i) => i)
-        .sort(() => 0.5 - Math.random())
-        .filter((el, i) => i < 5);
-      randomArray.forEach((el) => { this.recommend.push(data[el]); });
+      if (this.type === 'all') {
+        const { total } = resp.data.meta.pagination;
+        const randomArray = Array.from({ length: total })
+          .map((el, i) => i)
+          .sort(() => 0.5 - Math.random())
+          .filter((el, i) => i < 5);
+        randomArray.forEach((el) => { this.recommend.push(data[el]); });
+      } else {
+        const filterData = data.filter((el) => el.category === this.type);
+        const randomArray = Array.from({ length: filterData.length })
+          .map((el, i) => i)
+          .sort(() => 0.5 - Math.random())
+          .filter((el, i) => i < 5);
+        randomArray.forEach((el) => {
+          if (filterData[el].id !== this.id) {
+            this.recommend.push(filterData[el]);
+          }
+        });
+      }
+    },
+    goTo(path) {
+      if (this.$route.path.indexOf('product') > -1) {
+        this.$router.push(path);
+      } else {
+        this.$router.push(`product/${path}`);
+      }
     },
   },
 };
 </script>
 <style lang="sass" scoped>
-.card
+@mixin tablet
+  @media screen and (min-width: 576px) and (max-width: 767px)
+    @content
+@mixin desktop
+  @media screen and (min-width: 768px) and (max-width: 992px)
+    @content
+@mixin widescreen
+  @media screen and (min-width: 993px)
+    @content
+.rdn-card
   display: flex
   justify-content: center
   border: 1px solid transparent
@@ -69,6 +129,10 @@ export default {
       z-index: 2
       .des
         opacity: 1
+      +tablet
+        top: 8%
+      +desktop
+        top: 10%
   &::before
     content: ''
     position: absolute
@@ -76,7 +140,7 @@ export default {
     bottom: 0
     left: 0
     right: 0
-    background: rgba(#aaa , .8)
+    background: rgba(#aaa , .4)
     opacity: 0
     transition: .6s
     z-index: 1
@@ -94,7 +158,6 @@ export default {
     transition: .5s
     border-radius: 6px
     right: 0
-  &:hover::before
 .card-info
   align-self: flex-start
   position: absolute
@@ -109,4 +172,10 @@ export default {
     opacity: 0
     text-shadow: rgba(#ddd, .8) 0.1em 0.1em 1px
     transition: opacity 1s
+  +desktop
+    top: 74%
+  +tablet
+    top: 68%
+.cursor
+  cursor: pointer
 </style>
