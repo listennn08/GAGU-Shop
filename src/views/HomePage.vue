74507af2e4fabc2dba3a7138d89b093ef6e06007
listennn <listennn@Listens-MacBook-Pro.local>
2020-09-06 17:55:58 +0800

    adjust layout and fix rwd

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,49 +1,89 @@
 <template lang="pug">
   .container.is-fluid
     .container
-      .banner.banner-1
+      .banner.banner-1.desktop
         .image.is-3by1.is-fixed-left.img-1
           .text-in-img.left
             button.button.is-light.link(@click="goToProduct('bed')") 精選床墊
-        .box.is-fixed-right
+        .box.box-set.is-fixed-right.has-background-light
           .text
             span.logo GAGU
             | 照顧你的睡眠
             p 優質精選高級床墊，讓您睡到連地震都沒感覺
-      .banner
+      //- mobile
+      .banner.mobile
+        .image.is-2by1.img-1
+          .text-in-img.left.mobile
+            button.button.is-light.link(@click="goToProduct('bed')") 精選床墊
+        .box.box-set.has-background-light.mobile
+          .text.mobile
+            span.logo GAGU
+            | 照顧你的睡眠
+            p 優質精選高級床墊，讓您睡到連地震都沒感覺
+      .banner.desktop
         .image.is-3by1.is-fixed-right.img-2
           .text-in-img.right
             button.button.is-light.link(@click="goToProduct('sofa')") 熱銷沙發
-        .box.is-fixed-left
+        .box.box-set.is-fixed-left.has-background-light
           .text
             span.logo GAGU
             | 販售的沙發使用高級檜木打造而成
             p 可承受超過 1000 公斤的重量
-      .container.mt-5
-        .subtitle.is-4.cus-title.has-text-weight-bold.has-text-centered 精選推薦
-        .columns.is-multiline.is-gapless
-          .column.is-third-four(
-            v-for="data in random"
-            @click="$router.push(`product/${data.id}`)"
+      //- mobile
+      .banner.mobile
+        .image.is-2by1.img-2
+          .text-in-img.right.mobile
+            button.button.is-light.link(@click="goToProduct('sofa')") 熱銷沙發
+        .box.box-set.has-background-light.mobile
+          .text.mobile
+            span.logo GAGU
+            | 販售的沙發使用高級檜木打造而成
+            p 可承受超過 1000 公斤的重量
+      .banner.mt-5
+        RandomRecommend(
+          title="精選推薦"
+          type="all"
+          centered=true
+        )
+      .banner
+        .image.is-3by1.img-3
+        .box.box-set.is-shadowless.mt-1.has-background-transparent
+          .has-text-left
+            p 想了解更多 GAGU 的傢俱細節？
+            p 想知道更多關於 GAGU 的故事？
+            p 不想老是錯過 GAGU 的最新消息？
+            p 那就訂閱電子報吧！GAGU 的所有最新資訊都在這！
+          .field.has-addons.has-addons-centered.mt-1.is-fullwidth
+            .control.is-expanded
+              input.input(
+                placeholder="輸入電子郵件"
+                type="mail"
+                v-model="email"
+              )
+            .control
+              button.button.is-primary(
+                type="button"
+                @click="subcribe()"
+              ) 訂閱電子報
+        .in_right.desktop
+          button.is-outlinedless.is-rounded(
+            data-tooltip="鬼月不怕買傢俱！GAGU 全館五折起！\n折扣碼：7buymore"
+            :class="{'has-tooltip-active': showTooltip}"
+            @click="showTooltip = !showTooltip"
           )
-            .card
-              figure.image.1by1
-                img(:src="data.imageUrl[0]")
-              .card-info.mx-3
-                h4.subtitle.is-5.is-marginless.mh-70 {{ data.title }}
-                .has-text-primary
-                  span.is-font-raleway {{ data.price | cash }}
-                  del.ml-1: small.has-text-lightgray.is-font-raleway
-                    | {{ data.origin_price | cash }}
-                .fullheight.has-centered
-                  button.button.is-primary(@click.stop="$router.push(`product/${data.id}`)")
-                    |查看更多
+            font-awesome-icon(:icon="['fas', 'exclamation-circle']")
+        .in_right.mobile
+          button.is-outlinedless.is-rounded.has-tooltip-left(
+            data-tooltip="鬼月不怕買傢俱！GAGU 全館五折起！\n折扣碼：7buymore"
+            :class="{'has-tooltip-active': showTooltip}"
+            @click="showTooltip = !showTooltip"
+          )
+            font-awesome-icon(:icon="['fas', 'exclamation-circle']")
 </template>
 <script>
 import { mapActions } from 'vuex';
 import ProductCarousel from '@/components/ProductCarousel.vue';
 import RandomRecommend from '@/components/RandomRecommend.vue';
-import { getAllProducts } from '@/apis/frontend';
 
 export default {
   name: 'HomePage',
@@ -53,51 +93,33 @@ export default {
   },
   data() {
     return {
-      datas: [],
-      sales: [],
-      random: [],
-      s: null,
+      email: '',
+      showTooltip: true,
     };
-  },
-  async created() {
-    window.addEventListener('scroll', this.handleScroll);
-    const loader = this.$loading.show();
-    try {
-      const resp = await getAllProducts();
-      resp.data.data.forEach((item, index) => {
-        if (item.category === 'sofa' || item.category === 'chair') {
-          this.datas.push(item);
-        } else if (index < 10) {
-          this.sales.push(item);
-        }
-      });
-      while (this.random.length < 5) {
-        const n = Math.floor(Math.random() * (resp.data.data.length + 1) - 1);
-        if (this.random.findIndex((el) => el.id === resp.data.data[n].id) === -1) {
-          this.random.push(resp.data.data[n]);
-        }
-      }
-      loader.hide();
-    } catch {
-      loader.hide();
-    }
   },
   methods: {
     ...mapActions({
       setCategory: 'product/setCategory',
+      setMsg: 'setMsg',
     }),
     goToProduct(category) {
       this.setCategory(category);
       this.$router.push('products');
     },
-    handleScroll() {
-      // console.log(window.scrollY);
+    subcribe() {
+      if (this.email) {
+        this.setMsg({
+          msg: `使用 ${this.email} 訂閱電子報完成`,
+          type: true,
+        });
+      }
+      this.email = '';
     },
-
   },
 };
 </script>
 <style lang="sass" scoped>
+
 $navyblue: #333D51
 $hnavyblue: #242b39
 $goldyellow: #D3AC2B
@@ -107,22 +129,25 @@ $lightgray: #F4F3EA
 *
   font-family: 'Noto Sans TC', sans serif
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@500;900&family=Raleway:wght@700;900&family=Lobster&display=swap')
+@mixin tablet
+  @media screen and (min-width: 576px) and (max-width: 767px)
+    @content
+@mixin desktop
+  @media screen and (min-width: 768px)
+    @content
+@mixin widescreen
+  @media screen and (min-width: 993px)
+    @content
 
 $navyblue: #333D50
 $hnavyblue: #242b39
 .img-1
+  background: url('../static/banner-1.jpeg') center center / 100%
   position: relative
-  &::after
-    content: ''
-    background: url('https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80') center center / 100%
-    position: absolute
-    top: 0
-    right: 0
-    bottom: 0
-    left: 0
 .img-2
-  background: url('https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80') center center / 100%
-
+  background: url('../static/photo-1493663284031-b7e3aefcae8e.jpg') center center / 100%
+.img-3
+  background: url('../static/photo-1505620391902-46b120671eb6.jpg') center center / 100%
 .logo
   font-family: 'Lobster', sans serif
   margin-right: 1%
@@ -131,162 +156,101 @@ $hnavyblue: #242b39
   width: 100%
   box-sizing: border-box
   position: relative
-  margin: 5% 0
   display: flex
   align-items: center
   justify-content: center
   flex-direction: column
-  // border: 1px solid #dde
+  @media screen and ( max-width: 767px )
+    &.desktop
+      display: none
+  @media screen and ( min-width: 767px )
+    &.mobile
+      display: none
+  &:not(:last-of-type)
+    margin: 5% 0
   &-1
     transition: .5s
 .box
-  position: absolute
-  background-color: rgba(#F4F3EA, .8)
-  padding: 4% 2%
-  display: flex
-  align-items: center
-  color: $hnavyblue
+  &.box-set
+    position: absolute
+    padding: 4% 2%
+    display: flex
+    justify-content: center
+    flex-direction: column
+    color: $hnavyblue
   &.is-fixed-right
     top: 10%
     left: 50%
     right: 0%
     bottom: 70%
     text-align: left
+    +tablet
+      left: 40%
   &.is-fixed-left
     top: 10%
     left: 0%
     right: 50%
     bottom: 70%
     text-align: right
-    justify-content: flex-end
+    +tablet
+      right: 40%
+  &.mobile
+    top: 5%
+    opacity: .8
+    width: 100%
 .column
   cursor: pointer
 .image
-  width: 80%
+  width: 100%
   &.is-fixed-left
+    width: 80%
     align-self: flex-start
   &.is-fixed-right
+    width: 80%
     align-self: flex-end
 .text
   width: 100%
+  font-size: 16px
+  +tablet
+    font-size: 14px
 .text-in-img
   position: absolute
   top: 80%
+  &.mobile
+    top: 70%
+  +tablet
+    top: 70%
   z-index: 1
   &.left
     left: 5%
+  &.left.mobile
+    left: 50%
+    transform: translateX(-50%)
   &.right
     right: 5%
-.card
-  display: flex
-  justify-content: center
-  border: 1px solid transparent
-  overflow: hidden
-  &-image
-    transform: perspective(800px)
-    transform-style: preserve-3d
-  &:hover
-    border: 1px solid #ddd
-    border-radius: 6px
-    .card-info
-      top: 10%
-      z-index: 2
-      .des
-        opacity: 1
+  &.right.mobile
+    right: 50%
+    transform: translateX(50%)
+.in_right
+  position: fixed
+  top: 90%
+  z-index: 888
+  &.desktop
+    right: 120px
+  &.mobile
+    right: 10px
+.is-outlinedless
+  background: transparent
+  border: 0
+  outline: 0
+  font-size: 28px
+  color: #333d51
   &::before
-    content: ''
-    position: absolute
-    top: 0
-    bottom: 0
-    left: 0
-    right: 0
-    background: rgba(#aaa , .8)
-    opacity: 0
-    transition: .6s
-    z-index: 1
-  &:hover::before
+    margin-top: -5%
+  &.has-tooltip-active &::after
     opacity: 1
-.image
-  position: relative
-  cursor: pointer
-  &::before
-    content: ''
-    position: absolute
-    height: 100%
-    left: 0
-    right: 100%
-    transition: .5s
-    // background: rgba(#888, .5)
-    border-radius: 6px
-    right: 0
-  &:hover::before
-.card-info
-  align-self: flex-start
-  position: absolute
-  padding: 2%
-  top: 70%
-  bottom: 0
-  transition: top .5s
-  h4
-    text-shadow: rgba(#ddd, .8) 0.1em 0.1em 1px
-  .des
-    line-height: 20px
-    opacity: 0
-    text-shadow: rgba(#ddd, .8) 0.1em 0.1em 1px
-    transition: opacity 1s
-
-// .text
-//   color:  $navyblue
-//   position: absolute
-//   font-size: 24px
-// .text
-//   bottom: 25%
-//   left: 20%
-// .link
-//   position: absolute
-//   font-size: 16px
-//   bottom: 10%
-//   left: 2%
-// .cus-title
-//   color: $navyblue
-// .zoonin
-//   transition: transform .5s
-//   font-size: 1px
-//   &:hover
-//     transform: scale(1.8)
-//     box-shadow: 5px 5px 10px #888
-//     z-index: 999
-// .image
-//   background-size: 100%
-//   background-position: top center
-//   position: relative
-//   &:hover .txt
-//     background: rgba(#000, .8)
-//     opacity: 1
-//     transform: scale(1)
-// .txt
-//   // position: absolute
-//   height: 100%
-//   top: 0
-//   left: 0
-//   right: 0
-//   opacity: 0
-//   transition: .5s
-//   z-index: 999
-//   display: flex
-//   flex-direction: column
-//   .fullheight
-//     display: flex
-//     align-items: center
-//     justify-content: center
-//     align-self: flex-end
-//     width: 100%
-//     height: 100%
-// .is-fixed
-//   position: absolute
-//   opacity: .8
-//   top: 10%
-//   left: -5%
-//   right: 70%
-//   bottom: 10%
+  &:hover::after
+    opacity: 1
+  &:focus
+    outline: none
 </style>
